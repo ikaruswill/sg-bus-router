b268c23f848b8a4e35068397ee82ef07f54583cc
Will Ho <ikaruswill.ho@gmail.com>
2017-07-18 01:08:45 +0800

    Complete downloader script

--- downloader.py
+++ downloader.py
@@ -1,13 +1,35 @@
 import requests
-import json
+from time import sleep
+
+import pandas as pd
+from sqlalchemy import create_engine
+
 from keys import api_key
 
+LOG_INTERVAL = 1000
+
+db_conn = create_engine('sqlite:///sg-bus-routes.db')
 
 url = 'http://datamall2.mytransport.sg/ltaodataservice/BusRoutes'
+params = {
+    '$skip': 0
+}
 headers = {
     'AccountKey': api_key,
     'Accept': 'application/json'
 }
 
-json_str = requests.get(url=url, headers=headers).json()
-data_chunk = json.loads(json_str)['value']
+SKIP_INTERVAL = 50
+DB_TABLE = 'bus_routes'
+
+while True:
+    data_chunk = requests.get(url=url, headers=headers, params=params).json()['value']
+    if len(data_chunk) == 0:
+        break
+    df = pd.DataFrame(data_chunk)
+    df.to_sql(name=DB_TABLE, con=db_conn, if_exists='append')
+    params['$skip'] += SKIP_INTERVAL
+    if params['$skip'] % LOG_INTERVAL == 0:
+        print('Downloaded {}'.format(params['$skip']))
+    # Avoid triggering rate limits
+    # sleep(1)

caa521bf8a766fad4dfcac2c46e63de96070ebc7
Will Ho <ikaruswill.ho@gmail.com>
2017-07-28 00:51:12 +0800

    Simplify implementation to single destination for optimization

--- route.py
+++ route.py
@@ -7,6 +7,7 @@ from pprint import pprint
 import pandas as pd
 from sqlalchemy import create_engine
 
+# TODO: ALlow multiple destination nodes
 # TODO: Allow multiple solutions per destination. Currently takes the first optimal service stop as solution and ignores equally good routes
 # TODO: Allow multiple source nodes
 # TODO: Allow for multiple route suggestions without re-running algorithm
@@ -20,46 +21,18 @@ TRANSFER_PENALTY = 1
 
 @total_ordering
 class Node:
-    def __init__(self, bus_stop_code, service, best_cost=inf,
+    goal_stop = None
+
+    def __init__(self, bus_stop_code, service=None, service_no=None, best_cost=inf,
                  best_dist=inf, best_route=[]):
         self.bus_stop_code = bus_stop_code
         self.bus_stop = bs[bs.BusStopCode == self.bus_stop_code].iloc[0]
-        self.best_h_dist = inf
+        self.h_dist = self.calculate_heuristic()
         self.best_dist = best_dist
         self.best_cost = best_cost
         self.best_route = best_route
         self.service = service # Service stop
         self.services = rt[(rt.BusStopCode == self.bus_stop_code)]
-
-    def __lt__(self, other):
-        return self.best_cost < other.best_cost
-
-    def __hash__(self):
-        return hash('{} {}'.format(self.bus_stop_code, self.service))
-
-    def __repr__(self):
-        return '{} ({:>4}): {:>6.1f} | {:>6.1f} | {:>6.1f}km'.format(
-            self.bus_stop_code, self.service.ServiceNo, self.best_cost,
-            self.best_h_dist, self.best_dist)
-
-
-class Edge:
-    goals = {}
-
-    def __init__(self, source, service, dest):
-        self.source = source
-        self.service = service
-        self.dest = dest
-        self.distance = self.calculate_dist()
-        self.cost = self.calculate_cost()
-        self.update_dest_distance_cost_route()
-
-    def calculate_dist(self):
-        prev_service_stop = self.source.services[
-            (self.source.services.ServiceNo == self.service.ServiceNo) & \
-            (self.source.services.Direction == self.service.Direction) & \
-            (self.source.services.StopSequence == self.service.StopSequence - 1)].iloc[0]
-        return self.service.Distance - prev_service_stop.Distance
 
     def haversine(self, lon1, lat1, lon2, lat2):
         """
@@ -75,6 +48,39 @@ class Edge:
         c = 2 * asin(sqrt(a))
         km = 6367 * c
         return km
+
+    def calculate_heuristic(self):
+        return self.haversine(
+            self.bus_stop.Longitude, self.bus_stop.Latitude,
+            self.goal_stop.Longitude, self.goal_stop.Latitude)
+
+    def __lt__(self, other):
+        return self.best_cost < other.best_cost
+
+    def __hash__(self):
+        return hash('{} {}'.format(self.bus_stop_code, self.service))
+
+    def __repr__(self):
+        return '{} ({:>4}): {:>6.1f} | {:>6.1f} | {:>6.1f}km'.format(
+            self.bus_stop_code, self.service.ServiceNo, self.best_cost,
+            self.h_dist, self.best_dist)
+
+
+class Edge:
+    def __init__(self, source, service, dest):
+        self.source = source
+        self.service = service
+        self.dest = dest
+        self.distance = self.calculate_dist()
+        self.cost = self.calculate_cost()
+        self.update_dest_distance_cost_route()
+
+    def calculate_dist(self):
+        prev_service_stop = self.source.services[
+            (self.source.services.ServiceNo == self.service.ServiceNo) & \
+            (self.source.services.Direction == self.service.Direction) & \
+            (self.source.services.StopSequence == self.service.StopSequence - 1)].iloc[0]
+        return self.service.Distance - prev_service_stop.Distance
 
     def calculate_cost(self):
         cost = self.distance
@@ -92,14 +98,10 @@ class Edge:
     def update_dest_distance_cost_route(self):
         new_dist = self.source.best_dist + self.distance
         stops_per_km = new_dist/(len(self.source.best_route) + 1)
-        h_dist = min([self.haversine(
-            self.dest.bus_stop.Longitude, self.dest.bus_stop.Latitude,
-            goal.bus_stop.Longitude, goal.bus_stop.Latitude) for goal in self.goals.values()])
-        new_cost = self.source.best_cost + self.cost + stops_per_km + h_dist
+        new_cost = self.source.best_cost + self.cost + stops_per_km + self.dest.h_dist
         if new_cost < self.dest.best_cost:
             self.dest.best_cost = new_cost
             self.dest.best_dist = new_dist
-            self.dest.best_h_dist = h_dist
             self.dest.service = self.service
             self.dest.best_route = self.source.best_route + [self]
 
@@ -125,29 +127,23 @@ def discover_next_service_stops(node):
             next_service_stops.append(query.iloc[0])
     return next_service_stops
 
-def dijkstra(origin_code, goal_codes):
+def dijkstra(origin_code, goal_code):
     traversal_queue = []
     nodes = {}
     optimal_nodes = set()
-    goal_nodes = {}
-    soln_nodes = []
 
-    # Initialize goal nodes
-    for goal_code in goal_codes:
-        goal_services = rt[(rt.BusStopCode == goal_code)]
-        for goal_service in goal_services.itertuples():
-            goal = Node(goal_code, goal_service)
-            goal_nodes[goal_code] = goal
-    Edge.goals = goal_nodes
+    # Initialize goal node
+    Node.goal_stop = bs[(bs.BusStopCode == goal_code)]
 
     # Initialize origin node
     origin_services = rt[(rt.BusStopCode == origin_code)]
     for origin_service in origin_services.itertuples():
+        print(type(origin_service))
         origin = Node(origin_code, origin_service, 0, 0)
         traversal_queue.append(origin)
 
     # Dijkstra iterations
-    while len(goal_nodes):
+    while traversal_queue:
         current_node = heapq.heappop(traversal_queue)
         nodes[(current_node.bus_stop_code, current_node.service.ServiceNo)] = current_node
         optimal_nodes.add(current_node.bus_stop_code)
@@ -180,16 +176,15 @@ def dijkstra(origin_code, goal_codes):
             # print(' -', edge)
 
         # Store optimal route found for bus stop (Service agnostic)
-        if current_node.bus_stop_code in goal_nodes:
-            goal_nodes.pop(current_node.bus_stop_code)
-            soln_nodes.append(current_node)
+        if current_node.bus_stop_code == goal_code:
+            break
 
         # TODO: Could do with a little optimization, currently O(hn)
         # h = len(heapq), n = len(rt)
         # Maintain heap property in event node best cost has changed
         heapq.heapify(traversal_queue)
 
-    return soln_nodes
+    return current_node
 
 def main():
     global TRANSFER_PENALTY
@@ -200,40 +195,36 @@ def main():
     # Equally optimal   : 59039 -> 54589
     # Loops             : 11389 -> 11381
 
-    DEBUG_ORIGIN = '19051'
-    DEBUG_GOAL = ['18129']
+    DEBUG_ORIGIN = '59039'
+    DEBUG_GOAL = '54589'
 
     # Argument handling
     parser = ArgumentParser(
-        description='Finds the shortest bus route between a source and multiple '
-        'destination bus stops.')
+        description='Finds the shortest bus route between a source and a '
+        'destination bus stop.')
     parser.add_argument(
         '-t', '--transfer-penalty', default=TRANSFER_PENALTY,
         help="distance in km equivalent to the time & effort a transfer requires")
     parser.add_argument('-o', '--origin', help="origin bus stop code",
                         default=DEBUG_ORIGIN)
     parser.add_argument(
-        '-g', '--goals', nargs='*',
-        help="space-delimited acceptable destination bus stop codes",
-        default=DEBUG_GOAL)
+        '-g', '--goal', help="destination bus stop code", default=DEBUG_GOAL)
 
     args = parser.parse_args()
     TRANSFER_PENALTY = int(args.transfer_penalty)
     origin = args.origin
-    goals = args.goals
+    goal = args.goal
 
     # Fallback prompts
     if not origin:
         origin = input('Source bus stop code: ')
-    if not goals:
-        goals = input('Space-delimited destination bus-stop codes: ')
-        goals = [dest.strip() for dest in goals.split(',')]
+    if not goal:
+        goal = input('Destination bus-stop codes: ')
 
     # Run algorithm
-    solutions = dijkstra(origin, goals)
+    solution = dijkstra(origin, goal)
     print('Solution')
-    for solution in solutions:
-        pprint(solution.best_route)
+    pprint(solution.best_route)
 
 if __name__ == '__main__':
     main()

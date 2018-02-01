e4afad349d31024feb485cc06a53136ec2c7bdeb
Will Ho <ikaruswill.ho@gmail.com>
2018-02-02 00:55:04 +0800

    Adapt downloader to download multiple datasets

--- downloader.py
+++ downloader.py
@@ -7,8 +7,8 @@ from sqlalchemy import create_engine
 from keys import api_key
 
 # Variables
-API_PATH = 'BusStops'
-DB_TABLE = 'bus_stops'
+API_PATHS = ['BusStops', 'BusRoutes']
+DB_TABLES = ['bus_stops', 'bus_routes']
 
 # Options
 DB_PATH = 'sqlite:///sg-bus-router.db'
@@ -21,7 +21,6 @@ API_JSON_KEY = 'value'
 
 def main():
     db_conn = create_engine(DB_PATH)
-    url = API_URL_FORMAT.format(API_PATH)
     params = {
         '$skip': 0
     }
@@ -30,21 +29,24 @@ def main():
         'Accept': 'application/json'
     }
 
-    while True:
-        data_chunk = requests.get(
-            url=url, headers=headers, params=params).json()[API_JSON_KEY]
-        if len(data_chunk) == 0:
-            break
-        df = pd.DataFrame(data_chunk)
-        df.to_sql(name=DB_TABLE, con=db_conn, if_exists='append')
+    for i in range(len(API_PATHS)):
+        url = API_URL_FORMAT.format(API_PATHS[i])
 
-        # Log progress
-        params['$skip'] += API_SKIP_INTERVAL
-        if params['$skip'] % LOG_INTERVAL == 0:
-            print('Downloaded {}'.format(params['$skip']))
+        while True:
+            data_chunk = requests.get(
+                url=url, headers=headers, params=params).json()[API_JSON_KEY]
+            if len(data_chunk) == 0:
+                break
+            df = pd.DataFrame(data_chunk)
+            df.to_sql(name=DB_TABLES[i], con=db_conn, if_exists='append')
 
-        # # Avoid triggering rate limits
-        # sleep(1)
+            # Log progress
+            params['$skip'] += API_SKIP_INTERVAL
+            if params['$skip'] % LOG_INTERVAL == 0:
+                print('Downloaded {}'.format(params['$skip']))
+
+            # # Avoid triggering rate limits
+            # sleep(1)
 
 if __name__ == '__main__':
     main()

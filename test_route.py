b94a67bf7cc9b26553ddad6b9e30db17e3101110
Will Ho <ikaruswill.ho@gmail.com>
2018-03-15 20:22:50 +0800

    Added failure messages to test_within_dist

--- test_route.py
+++ test_route.py
@@ -23,10 +23,16 @@ class NearbyStopsTestCase(unittest.TestCase):
             with self.subTest(bus_stop_code=bus_stop_code):
                 # True Positive test: Correct bus stops included
                 if bus_stop_dict[self.dist_key] <= self.test_dist:
-                    self.assertIn(bus_stop_code, nearby_stops)
+                    self.assertIn(
+                        bus_stop_code, nearby_stops,
+                        msg='False negative.\nTest: <= {}  Excluded: {}'.format(
+                            self.test_dist, bus_stop_dict[self.dist_key]))
                 # True Negative test: Wrong bus stops excluded
                 elif bus_stop_dict[self.dist_key] > self.test_dist:
-                    self.assertNotIn(bus_stop_code, nearby_stops)
+                    self.assertNotIn(
+                        bus_stop_code, nearby_stops,
+                        msg='False positive.\nTest: <= {}  Included: {}'.format(
+                            self.test_dist, bus_stop_dict[self.dist_key]))
 
 
 if __name__ == '__main__':
